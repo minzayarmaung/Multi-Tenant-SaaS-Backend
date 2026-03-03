1507cd51a6933510ea4c4e269b53f9c2ce73af4e
Min Zayar Maung <minzayarmaung2002@gmail.com>
2026-03-03 08:32:21 +0630

    Updated

---
+++ src/main/java/com/project/Multi_Tenant_SaaS_Backend/MultiTenantSaaSBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.Multi_Tenant_SaaS_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSaaSBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSaaSBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Multi_Tenant_SaaS_Backend/MultiTenantSaaSBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Multi_Tenant_SaaS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSaaSBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
